48e177c30e2d470ca5da2cf31cde4f6919fc747a
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2024-06-19 21:30:02 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -55,23 +55,6 @@ async def delete_after_delay(message: Message, delay):
 
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
-    user_id = message.from_user.id
-    chat_id = -1002239078679 # The chat ID of your channel
-    bot_token = BOT_TOKEN  # Your bot token from the config
-
-    # Step 1: Make a request to the API to check if the user has joined the channel
-    api_url = f"https://api.jobians.top/telegram/getChatMember.php?bot_token={bot_token}&user_id={user_id}&chat_id={chat_id}"
-    response = requests.get(api_url)
-    data = response.json()
-
-    # Step 2: Check the response from the API
-    if data.get("status") == "true" and not data.get("is_joined"):
-        join_button = InlineKeyboardMarkup([[
-            InlineKeyboardButton('Join Channel', url='https://t.me/joinchat/YOUR_CHANNEL_LINK')
-        ]])
-        await message.reply("Please join the channel to use the bot.", reply_markup=join_button)
-        return
-        
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT.format(message.from_user.id, message.from_user.mention))
