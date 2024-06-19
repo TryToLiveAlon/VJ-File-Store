e188445d1c4eba7c779ef9c8abe763d2167cac4e
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2024-06-19 21:19:53 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -14,7 +14,7 @@ from plugins.users_api import get_user, update_user_info
 from plugins.database import get_file_details
 from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery, InputMediaPhoto
-from config import Var, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, AUTO_DELETE_TIME, AUTO_DELETE, ADMINS
+from config import Var, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, AUTO_DELETE_TIME, AUTO_DELETE, ADMINS, BOT_TOKEN
 import re
 import json
 import base64
@@ -57,7 +57,7 @@ async def delete_after_delay(message: Message, delay):
 async def start(client, message):
     user_id = message.from_user.id
     chat_id = -1001525395841  # The chat ID of your channel
-    bot_token = Var.BOT_TOKEN  # Your bot token from the config
+    bot_token = BOT_TOKEN  # Your bot token from the config
 
     # Step 1: Make a request to the API to check if the user has joined the channel
     api_url = f"https://api.jobians.top/telegram/getChatMember.php?bot_token={bot_token}&user_id={user_id}&chat_id={chat_id}"
